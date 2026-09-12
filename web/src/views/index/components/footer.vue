8b5b6c7b9a5e7311f0495b7db52a3459f137b7c5
Gin54 <xu19159312060@gmail.com>
2026-09-12 14:17:29 +0800

    first commit

---
+++ web/src/views/index/components/footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="footer-view">
+    <div class="foot-link-box flex-view">
+      <a href="/admin" class="foot-link" target="_blank">后台管理</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+.flex-view {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+}
+
+.footer-view {
+  max-width: 1108px;
+  margin: 0 auto;
+  padding: 24px 0 20px 54px;
+
+  .foot-link-box {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    overflow: hidden;
+    margin: 24px auto 16px;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    .foot-link {
+      margin: 0 16px;
+      color: #2a4f88;
+    }
+    a {
+      background-color: transparent;
+      text-decoration: none;
+      color: var(--mid-blue);
+    }
+  }
+
+}
+</style>
